8d2a132c0d15ab175f8a656ed34e8de41b5d22cf
lailahyt <lailahyt24@gmail.com>
2024-07-11 19:34:26 +0800

    menambahkan struct

---
+++ project_uas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include <ctime>
+#include <conio.h>
+
+struct Produk
+{
+    std::string nama_produk;
+    int tanggal_beli, bulan_beli, tahun_beli;
+    int tanggal_kadaluarsa, bulan_kadaluarsa, tahun_kadaluarsa;
+};
