fe559bfe6d4e170ecd150a6da861a02bd8de87e2
lailahyt <lailahyt24@gmail.com>
2024-07-11 19:39:32 +0800

    menambahkan void intro

--- project_uas.cpp
+++ project_uas.cpp
@@ -10,3 +10,13 @@ struct Produk
     int tanggal_beli, bulan_beli, tahun_beli;
     int tanggal_kadaluarsa, bulan_kadaluarsa, tahun_kadaluarsa;
 };
+
+void intro()
+{
+    system("cls");
+    std::cout << "===================================" << std::endl;
+    std::cout << "     Program Kadaluarsa Produk  " << std::endl;
+    std::cout << "===================================" << std::endl;
+    std::cout << "Tekan Enter untuk melanjutkan..." << std::endl;
+    std::cin.get();
+}
